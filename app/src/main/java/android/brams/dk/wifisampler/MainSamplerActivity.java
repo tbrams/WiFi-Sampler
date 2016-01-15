db5ccf08a7cb39228587019666a1bb14d31a0837
tbrams <torben@brams.dk>
2016-01-15 19:22:13 +0700

    Implemented part of the permission framework. Still not working though

--- app/src/main/java/android/brams/dk/wifisampler/MainSamplerActivity.java
+++ app/src/main/java/android/brams/dk/wifisampler/MainSamplerActivity.java
@@ -1,31 +1,52 @@
 package android.brams.dk.wifisampler;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainSamplerActivity extends AppCompatActivity {
     private static final String TAG = "MainSamplerActivity";
+    private static final int REQUEST_FINE_LOCATION=0;
+    private static boolean allClear=false;
 
     ListView lv;
     ImageView iv;
     WifiManager wifi;
     String wifis[], infos[];
     WifiScanReceiver wifiReciever;
+
+    private Toolbar mToolbar;
+    private CoordinatorLayout coordinatorLayout;
+    private Button btnSimpleSnackbar, btnActionCallback, btnCustomView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,26 +63,68 @@ public class MainSamplerActivity extends AppCompatActivity {
 
         });
 
-        wifi=(WifiManager)getSystemService(Context.WIFI_SERVICE);
-        wifiReciever = new WifiScanReceiver();
-        wifi.startScan();
+        if (ContextCompat.checkSelfPermission(MainSamplerActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            Log.i(TAG, "onCreate: We do not have the permission");
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainSamplerActivity.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Log.i(TAG, "onCreate: we should ask...");
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response!
+
+                // After the user sees the explanation, try again to request the permission.
+                Snackbar snackbar = Snackbar
+                        .make( coordinatorLayout , R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
+                        .setAction(android.R.string.ok, new View.OnClickListener() {
+                            @Override
+                            @TargetApi(Build.VERSION_CODES.M)
+                            public void onClick(View v) {
+                                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
+                            }
+                        });
+
+            } else {
+                Log.i(TAG, "onCreate: No need to ask, we can just request");
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(MainSamplerActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_FINE_LOCATION);
+
+                // REQUEST_FINE_LOCATON is an app-defined int constant.
+                // Use it in the callback method that gets the result of the request.
+            }
+        }
+
+        Log.i(TAG, "onCreate: after permission check");
 
         iv=(ImageView)findViewById(R.id.logo_image);
-        iv.setOnClickListener(new ImageView.OnClickListener(){
+        iv.setOnClickListener(new ImageView.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 toast("Scanning...");
-                lv.setAdapter(new ArrayAdapter<String>(getApplicationContext(), R.layout.listitem, new String[0]));
+                lv.setAdapter(new ArrayAdapter<>(getApplicationContext(), R.layout.listitem, new String[0]));
                 wifi.startScan();
 
 
             }
         });
+
+
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+
     }
 
     protected void onPause() {
-        unregisterReceiver(wifiReciever);
+        if (allClear)
+            unregisterReceiver(wifiReciever);
+
         super.onPause();
     }
 
@@ -83,7 +146,7 @@ public class MainSamplerActivity extends AppCompatActivity {
                 infos[i]=scanResult.toString();
                 Log.d(TAG, "Added to list: " + wifis[i]);
             }
-            lv.setAdapter(new ArrayAdapter<String>(getApplicationContext(), R.layout.listitem, wifis));
+            lv.setAdapter(new ArrayAdapter<>(getApplicationContext(), R.layout.listitem, wifis));
         }
     }
 
@@ -92,5 +155,34 @@ public class MainSamplerActivity extends AppCompatActivity {
         Toast.makeText(MainSamplerActivity.this,
                 String.format("%s", text), Toast.LENGTH_LONG)
                 .show();
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_FINE_LOCATION: {
+                Log.i(TAG, "onRequestPermissionsResult: case statement begin");
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Yeah - permission granted, we can sample away
+
+                    Log.i(TAG, "onRequestPermissionsResult: all clear set");
+
+                    allClear=true;
+                    wifi=(WifiManager)getSystemService(Context.WIFI_SERVICE);
+                    wifiReciever = new WifiScanReceiver();
+                    wifi.startScan();
+
+                } else {
+                    Toast.makeText(MainSamplerActivity.this, "Not permitted to scan WiFi", Toast.LENGTH_SHORT).show();
+                }
+                return;
+
+            // other 'case' lines to check for other permissions this app might request
+            }
+        }
     }
 }
